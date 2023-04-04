2edcd9dd95dea39e5a6f8106ec3d9ccb77a933b7
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-04-04 23:12:47 +0430

    Create calculator.js file and completed the designe of calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+import './Calculator.css';
+const Calc = () => {
+    return (
+        <div className="calcContainer">
+            <span className="result">0</span>
+            <div className="row">
+                <button type="button" className="btn">AC</button>
+                <button type="button" className="btn">+/-</button>
+                <button type="button" className="btn">%</button>
+                <button type="button" className="btn btnLast">÷</button>
+            </div>
+            <div className="row">
+                <button type="button" className="btn">7</button>
+                <button type="button" className="btn">8</button>
+                <button type="button" className="btn">9</button>
+                <button type="button" className="btn btnLast">x</button>
+            </div>
+            <div className="row">
+                <button type="button" className="btn">4</button>
+                <button type="button" className="btn">5</button>
+                <button type="button" className="btn">6</button>
+                <button type="button" className="btn btnLast">-</button>
+            </div>
+            <div className="row">
+                <button type="button" className="btn">1</button>
+                <button type="button" className="btn">2</button>
+                <button type="button" className="btn">3</button>
+                <button type="button" className="btn btnLast">+</button>
+            </div>
+            <div className="row">
+                <button type="button" className="btn zero">0</button>
+                <button type="button" className="btn dot">.</button>
+                <button type="button" className="btn btnLast">=</button>
+            </div>
+        </div>
+    );
+}
+
+export default Calc;
