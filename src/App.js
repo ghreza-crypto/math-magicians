c9489eeddffcc4aba6f4cc0f2e69d6aa3b2935e1
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-04-12 04:05:56 +0430

    Add home page, quote and calculator routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import ShowQuote from './components/ShowQuote';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="container">
-      <Calculator />
-      <ShowQuote />
+      <nav>
+        <h1>Math Magicians</h1>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/calculator">Calculator</NavLink>
+          </li>
+          <li>
+            <NavLink to="/quote">Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<ShowQuote />} />
+      </Routes>
     </div>
   );
 }
