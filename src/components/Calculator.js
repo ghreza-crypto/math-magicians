811948ad35528578d885728e7a2aa3701c3ead41
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-04-12 19:59:02 +0430

    add calculator title to its componant

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,36 +8,39 @@ const Calc = () => {
   };
 
   return (
-    <div className="calcContainer">
-      <span className="result">{result.next || result.total || 0}</span>
-      <div className="row">
-        <button type="button" className="btn" onClick={onClickHandler}>AC</button>
-        <button type="button" className="btn" onClick={onClickHandler}>+/-</button>
-        <button type="button" className="btn" onClick={onClickHandler}>%</button>
-        <button type="button" className="btn btnLast" onClick={onClickHandler}>÷</button>
-      </div>
-      <div className="row">
-        <button type="button" className="btn" onClick={onClickHandler}>7</button>
-        <button type="button" className="btn" onClick={onClickHandler}>8</button>
-        <button type="button" className="btn" onClick={onClickHandler}>9</button>
-        <button type="button" className="btn btnLast" onClick={onClickHandler}>x</button>
-      </div>
-      <div className="row">
-        <button type="button" className="btn" onClick={onClickHandler}>4</button>
-        <button type="button" className="btn" onClick={onClickHandler}>5</button>
-        <button type="button" className="btn" onClick={onClickHandler}>6</button>
-        <button type="button" className="btn btnLast" onClick={onClickHandler}>-</button>
-      </div>
-      <div className="row">
-        <button type="button" className="btn" onClick={onClickHandler}>1</button>
-        <button type="button" className="btn" onClick={onClickHandler}>2</button>
-        <button type="button" className="btn" onClick={onClickHandler}>3</button>
-        <button type="button" className="btn btnLast" onClick={onClickHandler}>+</button>
-      </div>
-      <div className="row">
-        <button type="button" className="btn zero" onClick={onClickHandler}>0</button>
-        <button type="button" className="btn dot" onClick={onClickHandler}>.</button>
-        <button type="button" className="btn btnLast" onClick={onClickHandler}>=</button>
+    <div className="pageContainer">
+      <div><h2>Lets do some math!</h2></div>
+      <div className="calcContainer">
+        <span className="result">{result.next || result.total || 0}</span>
+        <div className="row">
+          <button type="button" className="btn" onClick={onClickHandler}>AC</button>
+          <button type="button" className="btn" onClick={onClickHandler}>+/-</button>
+          <button type="button" className="btn" onClick={onClickHandler}>%</button>
+          <button type="button" className="btn btnLast" onClick={onClickHandler}>÷</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={onClickHandler}>7</button>
+          <button type="button" className="btn" onClick={onClickHandler}>8</button>
+          <button type="button" className="btn" onClick={onClickHandler}>9</button>
+          <button type="button" className="btn btnLast" onClick={onClickHandler}>x</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={onClickHandler}>4</button>
+          <button type="button" className="btn" onClick={onClickHandler}>5</button>
+          <button type="button" className="btn" onClick={onClickHandler}>6</button>
+          <button type="button" className="btn btnLast" onClick={onClickHandler}>-</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={onClickHandler}>1</button>
+          <button type="button" className="btn" onClick={onClickHandler}>2</button>
+          <button type="button" className="btn" onClick={onClickHandler}>3</button>
+          <button type="button" className="btn btnLast" onClick={onClickHandler}>+</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn zero" onClick={onClickHandler}>0</button>
+          <button type="button" className="btn dot" onClick={onClickHandler}>.</button>
+          <button type="button" className="btn btnLast" onClick={onClickHandler}>=</button>
+        </div>
       </div>
     </div>
   );
